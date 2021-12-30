69c68544ee20a88340981e0d70a67a8be5c8c964
Charlesyim <yanchicharles@gmail.com>
2021-12-30 22:06:46 +0800

    7

--- src/main/java/com/charles/zlbbs/controller/GlobalExceptionHandler.java
+++ src/main/java/com/charles/zlbbs/controller/GlobalExceptionHandler.java
@@ -12,9 +12,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
 
 @ControllerAdvice
 @Slf4j
